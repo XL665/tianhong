da9a04ee8d4f7fb72edf4006f53a00d5ff60e763
XL665 <54076062+XL665@users.noreply.github.com>
2021-05-26 09:49:56 +0800

    Create user.js

---
+++ data/data/user.js
@@ -0,0 +1,76 @@
+https://www.tianhong.cn    
+后台用户数据:http://192.168.197.62:9000/ 
+username:root 
+password:root@123&^%$ 
+据库mysql username:root 
+password:tianhong@root#@$%
+
+onst data = {
+  "sku": [
+      {
+          "sku_id": 6098336,
+          "sku_code": "6098336",
+          "sku_name": "Apple iPhone 11 移动联通电信4G手机 双卡双待-黑色-64GB-全网通",
+          "sale_price": 5499,
+          "sale_integral": "137.48",
+          "market_price": "5499",
+          "sku_num": 38,
+          "skuspec_data": "颜色:黑色;手机内存:64GB;手机网络制式:全网通;",
+          "images": [
+              "https://img1.tianhong.cn/upload/pd/m/shouji/20199/LTZVTQ7RCBYVAIFEFrtgqQ4S_390x390.jpg",
+              "https://img1.tianhong.cn/upload/pd/m/shouji/20199/4rIVH5uYxhezmYoLBjxfGy9j_390x390.jpg"
+          ],
+          "large_images": [
+              "https://img1.tianhong.cn/upload/pd/m/shouji/20199/LTZVTQ7RCBYVAIFEFrtgqQ4S_600x600.jpg",
+              "https://img1.tianhong.cn/upload/pd/m/shouji/20199/4rIVH5uYxhezmYoLBjxfGy9j_600x600.jpg"
+          ],
+          "integral": "137.48",
+          "discount": null,
+          "realProductPrice": 5499,
+          "dis": 1
+      },
+      {
+          "sku_id": 6098337,
+          "sku_code": "6098337",
+          "sku_name": "Apple iPhone 11 移动联通电信4G手机 双卡双待-白色-64GB-全网通",
+          "sale_price": 5499,
+          "sale_integral": "137.48",
+          "market_price": "5499",
+          "sku_num": 49,
+          "skuspec_data": "颜色:白色;手机内存:64GB;手机网络制式:全网通;",
+          "images": [
+              "https://img1.tianhong.cn/upload/pd/m/shouji/20199/1pOVyXqX77k63g8odTDJNSjJ_390x390.jpg",
+              "https://img1.tianhong.cn/upload/pd/m/shouji/20199/lG4ldRXq5VgBDtupb8YT0rzk_390x390.jpg"
+          ],
+          "large_images": [
+              "https://img1.tianhong.cn/upload/pd/m/shouji/20199/1pOVyXqX77k63g8odTDJNSjJ_600x600.jpg",
+              "https://img1.tianhong.cn/upload/pd/m/shouji/20199/lG4ldRXq5VgBDtupb8YT0rzk_600x600.jpg"
+          ],
+          "integral": "137.48",
+          "discount": null,
+          "realProductPrice": 5499,
+          "dis": 1
+      },
+      {
+          "sku_id": 6098338,
+          "sku_code": "6098338",
+          "sku_name": "Apple iPhone 11 移动联通电信4G手机 双卡双待-红色-64GB-全网通",
+          "sale_price": 5499,
+          "sale_integral": "137.48",
+          "market_price": "5499",
+          "sku_num": 49,
+          "skuspec_data": "颜色:红色;手机内存:64GB;手机网络制式:全网通;",
+          "images": [
+              "https://img1.tianhong.cn/upload/pd/m/shouji/20199/6CUfH232Fqc5IFHSgc1tpgTs_390x390.jpg",
+              "https://img1.tianhong.cn/upload/pd/m/shouji/20199/BQz8Hs527l6PPr6l1LdYguD3_390x390.jpg"
+          ],
+          "large_images": [
+              "https://img1.tianhong.cn/upload/pd/m/shouji/20199/6CUfH232Fqc5IFHSgc1tpgTs_600x600.jpg",
+              "https://img1.tianhong.cn/upload/pd/m/shouji/20199/BQz8Hs527l6PPr6l1LdYguD3_600x600.jpg"
+          ],
+          "integral": "137.48",
+          "discount": null,
+          "realProductPrice": 5499,
+          "dis": 1
+      },
+      {
